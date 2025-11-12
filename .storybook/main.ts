4beb8c1868ba2c5cae2304ef1b2573ccfb180161
Aneek Mukhopadhyay <dream.aneek@gmail.com>
2025-11-13 00:04:50 +0100

    refactor: Add more configuration for ddev.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,16 @@ const config: StorybookConfig = {
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
-  }
+  },
+  viteFinal: (config) => {
+    config.server = config.server || {};
+    config.server.allowedHosts = [
+      '0.0.0.0',
+      'localhost',
+      process.env.DDEV_HOSTNAME || '',
+      process.env.DDEV_PRIMARY_URL || '',
+    ].filter(host => host !== '');
+    return config;
+  },
 };
 export default config;
